9ca4a5afeffa3bfee840e17ec4d61d6d6a0feb1e
mario kennedy <mk@mariokennedy.com>
2019-08-27 16:50:06 -0700

    refactors logic and adds custom name cases

--- index.js
+++ index.js
@@ -1,9 +1,75 @@
 const NAME_MAX_LENGTH = 18;
 
-module.exports = shortenLongName = fullname => {
-  if (fullname.length < NAME_MAX_LENGTH) return fullname;
-  const parsedName = fullname.split(' ').filter(name => name !== '')
-  const firstName = parsedName[0]
-  const lastName = `${parsedName[parsedName.length - 1][0]}.`
-  return `${firstName} ${lastName}`
+module.exports = shortenLongName = (firstName, lastName) => {
+  
+  //Name Arrays
+  const firstNameParsed = firstName.split(' ').filter(name => name !== '') 
+  const lastNameParsed = lastName.split(' ').filter(name => name !== '')
+  const fullNameParsed = [...firstNameParsed, ...lastNameParsed]
+  //Name Variations
+  const fullName = `${firstName} ${lastName}`
+
+  if (fullName.length < NAME_MAX_LENGTH) return fullName;
+
+  if(fullNameParsed.length === 2){
+    return `${firstName} ${lastName.charAt(0)}.`
+  }
+
+  if(firstNameParsed.length > 1 && lastNameParsed.length < 2){
+    let fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}` // Mario D. Kennedy
+    let fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt(0)}.` // Mario Daniel K.
+    let noMiddleFullLast = `${firstNameParsed[0]} ${lastName}` // Mario Kennedy
+
+    let names = [
+      fullLastMiddleInitial,
+      fullMiddleSingleLastInitial,
+      fullMiddleDoubleLastInitial,
+      noMiddleFullLast
+    ]
+    return closetToMax(names)
+  }
+
+  if(firstNameParsed.length < 2 && lastNameParsed.length > 1){
+    let fullFirstdoubleLastInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+    let noMiddleDoubleInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+    let noMiddleSingleInitial = `${firstName} ${lastNameParsed[0][0]}.`
+
+    let names = [
+      fullFirstdoubleLastInitial,
+      noMiddleDoubleInitial,
+      noMiddleSingleInitial
+    ]
+    return closetToMax(names)
+  }
+
+  if(fullNameParsed.length > 3){
+    let fullMiddleDoubleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastNameParsed[0].charAt(0)}. ${lastNameParsed[1][0]}.` // Mario Daniel K. K.
+    let fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}` // Mario D. Kennedy Kavouras
+    let noMiddleFullLast = `${firstNameParsed[0]} ${lastName}` // Mario Kennedy Kavouras
+    let fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt(0)}.` // Mario Daniel K.
+    let noMiddleDoubleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+    let noMiddleSingleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}.`
+	let tripleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+
+    let names = [
+      fullLastMiddleInitial,
+      fullMiddleDoubleLastInitial,
+      fullMiddleSingleLastInitial,
+      noMiddleFullLast,
+      tripleInitial,
+      noMiddleDoubleInitial,
+      noMiddleSingleInitial
+    ]
+    return closetToMax(names)
+  }
+}   
+
+const closetToMax = (arr) => {
+  debugger
+  let shortName = ''
+
+  const shortNames = arr.filter(name => name.length <= NAME_MAX_LENGTH)
+  const nameLengths = shortNames.map(n => n.length)
+
+  return shortNames[nameLengths.indexOf(Math.max(...nameLengths))]
 }
