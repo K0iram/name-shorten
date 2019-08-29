1d5f8bc2e6236a003c3c355de6c71f636bdf4657
mario kennedy <mk@mariokennedy.com>
2019-08-28 20:49:13 -0700

    moves capitalize function into main file.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import capitalize from './utils/capitalize'
-const NAME_MAX_LENGTH = 18;
+const NAME_MAX_LENGTH = 18
 
-module.exports = shortenLongName = (firstName, lastName) => {
+module.exports = nameShorten = (firstName, lastName) => {
   
   //Name Arrays
   const firstNameParsed = firstName.split(' ').filter(name => name !== '') 
@@ -17,11 +16,11 @@ module.exports = shortenLongName = (firstName, lastName) => {
   }
 
   if(firstNameParsed.length > 1 && lastNameParsed.length < 2){
-    let fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}` // Mario D. Kennedy
-    let fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt(0)}.` // Mario Daniel K.
-    let noMiddleFullLast = `${firstNameParsed[0]} ${lastName}` // Mario Kennedy
+    const fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}` // Mario D. Kennedy
+    const fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt(0)}.` // Mario Daniel K.
+    const noMiddleFullLast = `${firstNameParsed[0]} ${lastName}` // Mario Kennedy
 
-    let names = [
+    const names = [
       fullLastMiddleInitial,
       fullMiddleSingleLastInitial,
       noMiddleFullLast
@@ -30,11 +29,11 @@ module.exports = shortenLongName = (firstName, lastName) => {
   }
 
   if(firstNameParsed.length < 2 && lastNameParsed.length > 1){
-    let fullFirstdoubleLastInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
-    let noMiddleDoubleInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
-    let noMiddleSingleInitial = `${firstName} ${lastNameParsed[0][0]}.`
+    const fullFirstdoubleLastInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+    const noMiddleDoubleInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+    const noMiddleSingleInitial = `${firstName} ${lastNameParsed[0][0]}.`
 
-    let names = [
+    const names = [
       fullFirstdoubleLastInitial,
       noMiddleDoubleInitial,
       noMiddleSingleInitial
@@ -43,15 +42,15 @@ module.exports = shortenLongName = (firstName, lastName) => {
   }
 
   if(fullNameParsed.length > 3){
-    let fullMiddleDoubleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastNameParsed[0].charAt(0)}. ${lastNameParsed[1][0]}.` // Mario Daniel K. K.
-    let fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}` // Mario D. Kennedy Kavouras
-    let noMiddleFullLast = `${firstNameParsed[0]} ${lastName}` // Mario Kennedy Kavouras
-    let fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt(0)}.` // Mario Daniel K.
-    let noMiddleDoubleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
-    let noMiddleSingleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}.`
-	  let tripleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+    const fullMiddleDoubleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastNameParsed[0].charAt(0)}. ${lastNameParsed[1][0]}.` // Mario Daniel K. K.
+    const fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}` // Mario D. Kennedy Kavouras
+    const noMiddleFullLast = `${firstNameParsed[0]} ${lastName}` // Mario Kennedy Kavouras
+    const fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt(0)}.` // Mario Daniel K.
+    const noMiddleDoubleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+    const noMiddleSingleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}.`
+	  const tripleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
 
-    let names = [
+    const names = [
       fullLastMiddleInitial,
       fullMiddleDoubleLastInitial,
       fullMiddleSingleLastInitial,
@@ -64,8 +63,12 @@ module.exports = shortenLongName = (firstName, lastName) => {
   }
 }   
 
+const capitalize = str => {
+  let parts = str.split(' ')
+  return parts.map(n => `${n.charAt(0).toUpperCase()}${n.slice(1).toLowerCase()}`).join(' ')
+}
+
 const closetToMax = (arr) => {
-  
   const shortNames = arr.filter(name => name.length <= NAME_MAX_LENGTH)
   const nameLengths = shortNames.map(n => n.length)
   let shortName = shortNames[nameLengths.indexOf(Math.max(...nameLengths))]
--- utils/capitalize.js
+++
@@ -1,6 +0,0 @@
-const capitalize = str => {
-    let parts = str.split(' ')
-    return parts.map(n => `${n.charAt(0).toUpperCase()}${n.slice(1).toLowerCase()}`).join(' ')
-}
-
-export default capitalize
