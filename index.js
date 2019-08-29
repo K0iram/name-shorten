9758ed323df92bb59e40d9da9dc818a670a08f04
mario kennedy <mk@mariokennedy.com>
2019-08-28 21:00:36 -0700

    prettier format and add caplitalize to 2 more conditions

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-const NAME_MAX_LENGTH = 18
+const NAME_MAX_LENGTH = 18;
 
 module.exports = nameShorten = (firstName, lastName) => {
-  
   //Name Arrays
-  const firstNameParsed = firstName.split(' ').filter(name => name !== '') 
-  const lastNameParsed = lastName.split(' ').filter(name => name !== '')
-  const fullNameParsed = [...firstNameParsed, ...lastNameParsed]
+  const firstNameParsed = firstName.split (' ').filter (name => name !== '');
+  const lastNameParsed = lastName.split (' ').filter (name => name !== '');
+  const fullNameParsed = [...firstNameParsed, ...lastNameParsed];
   //Name Variations
-  const fullName = `${firstName} ${lastName}`
+  const fullName = `${firstName} ${lastName}`;
 
-  if (fullName.length < NAME_MAX_LENGTH) return fullName;
+  if (fullName.length < NAME_MAX_LENGTH) return capitalize(fullName);
 
-  if(fullNameParsed.length === 2){
-    return `${firstName} ${lastName.charAt(0)}.`
+  if (fullNameParsed.length === 2) {
+    return capitalize(`${firstName} ${lastName.charAt (0)}.`);
   }
 
-  if(firstNameParsed.length > 1 && lastNameParsed.length < 2){
-    const fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}` // Mario D. Kennedy
-    const fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt(0)}.` // Mario Daniel K.
-    const noMiddleFullLast = `${firstNameParsed[0]} ${lastName}` // Mario Kennedy
+  if (firstNameParsed.length > 1 && lastNameParsed.length < 2) {
+    const fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}`; // Mario D. Kennedy
+    const fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt (0)}.`; // Mario Daniel K.
+    const noMiddleFullLast = `${firstNameParsed[0]} ${lastName}`; // Mario Kennedy
 
     const names = [
       fullLastMiddleInitial,
       fullMiddleSingleLastInitial,
-      noMiddleFullLast
-    ]
-    return closetToMax(names)
+      noMiddleFullLast,
+    ];
+    return closetToMax (names);
   }
 
-  if(firstNameParsed.length < 2 && lastNameParsed.length > 1){
-    const fullFirstdoubleLastInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
-    const noMiddleDoubleInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
-    const noMiddleSingleInitial = `${firstName} ${lastNameParsed[0][0]}.`
+  if (firstNameParsed.length < 2 && lastNameParsed.length > 1) {
+    const fullFirstdoubleLastInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`;
+    const noMiddleDoubleInitial = `${firstName} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`;
+    const noMiddleSingleInitial = `${firstName} ${lastNameParsed[0][0]}.`;
 
     const names = [
       fullFirstdoubleLastInitial,
       noMiddleDoubleInitial,
-      noMiddleSingleInitial
-    ]
-    return closetToMax(names)
+      noMiddleSingleInitial,
+    ];
+    return closetToMax (names);
   }
 
-  if(fullNameParsed.length > 3){
-    const fullMiddleDoubleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastNameParsed[0].charAt(0)}. ${lastNameParsed[1][0]}.` // Mario Daniel K. K.
-    const fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}` // Mario D. Kennedy Kavouras
-    const noMiddleFullLast = `${firstNameParsed[0]} ${lastName}` // Mario Kennedy Kavouras
-    const fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt(0)}.` // Mario Daniel K.
-    const noMiddleDoubleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
-    const noMiddleSingleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}.`
-	  const tripleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`
+  if (fullNameParsed.length > 3) {
+    const fullMiddleDoubleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastNameParsed[0].charAt (0)}. ${lastNameParsed[1][0]}.`; // Mario Daniel K. K.
+    const fullLastMiddleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastName}`; // Mario D. Kennedy Kavouras
+    const noMiddleFullLast = `${firstNameParsed[0]} ${lastName}`; // Mario Kennedy Kavouras
+    const fullMiddleSingleLastInitial = `${firstNameParsed[0]} ${firstNameParsed[1]} ${lastName.charAt (0)}.`; // Mario Daniel K.
+    const noMiddleDoubleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`;
+    const noMiddleSingleInitial = `${firstNameParsed[0]} ${lastNameParsed[0][0]}.`;
+    const tripleInitial = `${firstNameParsed[0]} ${firstNameParsed[1][0]}. ${lastNameParsed[0][0]}. ${lastNameParsed[1][0]}.`;
 
     const names = [
       fullLastMiddleInitial,
@@ -57,21 +56,23 @@ module.exports = nameShorten = (firstName, lastName) => {
       noMiddleFullLast,
       tripleInitial,
       noMiddleDoubleInitial,
-      noMiddleSingleInitial
-    ]
-    return closetToMax(names)
+      noMiddleSingleInitial,
+    ];
+    return closetToMax (names);
   }
-}   
+};
 
 const capitalize = str => {
-  let parts = str.split(' ')
-  return parts.map(n => `${n.charAt(0).toUpperCase()}${n.slice(1).toLowerCase()}`).join(' ')
-}
+  let parts = str.split (' ');
+  return parts
+    .map (n => `${n.charAt (0).toUpperCase ()}${n.slice (1).toLowerCase ()}`)
+    .join (' ');
+};
 
-const closetToMax = (arr) => {
-  const shortNames = arr.filter(name => name.length <= NAME_MAX_LENGTH)
-  const nameLengths = shortNames.map(n => n.length)
-  let shortName = shortNames[nameLengths.indexOf(Math.max(...nameLengths))]
+const closetToMax = arr => {
+  const shortNames = arr.filter (name => name.length <= NAME_MAX_LENGTH);
+  const nameLengths = shortNames.map (n => n.length);
+  let shortName = shortNames[nameLengths.indexOf (Math.max (...nameLengths))];
 
-  return capitalize(shortName)
-}
+  return capitalize (shortName);
+};
